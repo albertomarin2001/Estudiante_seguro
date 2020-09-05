c9f9df62a79b88a5f106218d52b1ec1b2b403629
Alberto Marin Estremadoiro <a2019116197@estudiantes.upsa.edu.bo>
2020-09-05 11:56:37 -0400

    Se agrego el proyecto de administracion MVC

---
+++ APIStudentSecurity/admStudentSecurity/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace admStudentSecurity.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+
+        [StringLength(30)]
+        [Required(ErrorMessage = "You Must Enter The Field {0}")]
+        public string LastName { get; set; }
+
+
+        [StringLength(30, ErrorMessage = "The Field {0} Must Contain Between {2} and {1} Characters", MinimumLength = 2)]// EL 0 ES LA REFERENCIA
+        [Required(ErrorMessage = "You Must Enter The Field {0}")]
+        public string FirstName { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
